ea74990ef1d523c649b83d433968f264344ef12f
DMalko <bionet@inbox.ru>
2014-03-04 22:41:15 +0400

    home page with some app controllers

--- wordmedium_app/lib/WordMedium.pm
+++ wordmedium_app/lib/WordMedium.pm
@@ -1,18 +1,155 @@
 package WordMedium;
+
 use Mojo::Base 'Mojolicious';
+# Dependences:
+# 1. Mojolicious::Plugin::Database
+# 2. Mojolicious::Plugin::Authentication
+# 3. Mojolicious::Plugin::Bcrypt
+# 4. Mojolicious::Plugin::Mail
+
+# App domain
+my $domain = 'wordmedium.com';
+
+# App mails
+my $noreplay_mail = 'noreplay@wordmedium.com';
+
+# Database parameters
+my $dbname = 'wmdb';
+my $dbhost = 'localhost';
+my $dbport = '3306';
+my $dbuser = 'root';
+my $dbpass = '';
 
 # This method will run once at server start
 sub startup {
-  my $self = shift;
+    my $self = shift;
+    
+    $self->secrets(['In vino veritas, in aqua sanitas!']);
+    
+    # Documentation browser under "/perldoc" #
+    ##########################################
+    $self->plugin('PODRenderer');
+    
+    # Sendmail helper `mail` #
+    ###########################
+    $self->plugin(mail => {
+        from => $noreplay_mail,
+        type => 'text/plain',
+    });
+    
+    # Database helper `db` #
+    ########################
+    my $dsn = "dbi:mysql:$dbname:$dbhost:$dbport";
+    $self->plugin('database', { 
+        dsn      => $dsn,
+        username => $dbuser,
+        password => $dbpass,
+        options  => {RaiseError => 1, AutoCommit => 1, mysql_enable_utf8 => 1, mysql_auto_reconnect => 1},
+        helper   => 'db'
+    });
+    
+    # Use strong encryption #
+    #########################
+    $self->plugin('bcrypt', {cost => 6});
+    
+    # Authentication #
+    ##################
+    $self->plugin('authentication', {
+        'autoload_user' => 1,
+        'session_key' => 'auth_data',
+        'load_user' => sub {
+            my $self = shift;
+            my $uid  = shift;
+            return {uid => $uid};
+        },
+        'validate_user' => sub {
+            my ($self, $user, $password, $extradata) = @_;
 
-  # Documentation browser under "/perldoc"
-  $self->plugin('PODRenderer');
-
-  # Router
-  my $r = $self->routes;
-
-  # Normal route to controller
-  $r->get('/')->to('home#index');
+            my $getuid = $self->db->prepare("SELECT uid, pass FROM user WHERE uname = ? || email = ?");
+            $getuid->execute($user, $user);
+            my ($uid, $pass) = $getuid->fetchrow_array();
+            return $uid if $self->bcrypt_validate($password, $pass);
+            return undef;
+        }
+    });
+    
+    # Authentication helpers #
+    ##########################
+    # usage:
+    # my $user_hash = $self->uid2user($uid);
+    $self->helper(uid2user => sub {
+        my $self = shift;
+        my $uid = shift;
+        
+        my $get_user = $self->db->prepare(q{SELECT uname, email, created FROM user WHERE uid = ?});
+	$get_user->execute($uid);
+	my ($uname, $email, $time) = $get_user->fetchrow_array();
+        return {uname => $uname, email => $email, timestamp => $time} if $uname && $email;
+        return undef;
+    });
+    
+    # usage:
+    # my $uid = $self->user2uid('user_name');
+    # my $uid = $self->user2uid('user@email.com');
+    $self->helper(user2uid => sub {
+        my $self = shift;
+        my $user = shift;
+        
+        return undef unless $user;
+        my $get_uid = $self->db->prepare(q{SELECT uid FROM user WHERE uname = ? OR email = ?});
+	$get_uid->execute($user, $user);
+	my ($uid) = $get_uid->fetchrow_array();
+        return $uid;
+    });
+    
+    # usage:
+    # my $domain = $self->mydomain();
+    $self->helper(mydomain => sub {
+        my $self = shift;
+        return $domain;
+    });
+    
+    # usage:
+    # my $password = $self->randpass($pass_length);
+    $self->helper(randpass => sub {
+        my $self = shift;
+        my $length = shift;
+        
+        # Avoids using confusing characters such as lower case L (l) and the number one (1), the letter 'O' and the number zero.
+        my @chars = ('A'..'N','P'..'Z','a'..'k','m','n','p'..'z','2'..'9');
+        my $password = '';
+        for (0..$length) {
+            $password .= $chars[int rand @chars];
+        }
+        return $password;
+    });
+    
+    # usage:
+    # $self->passreset($uid, $new_password);
+    $self->helper(passreset => sub {
+        my $self = shift;
+        my $uid = shift;
+        my $pass = shift;
+        
+        my $crptpass = $self->bcrypt($pass);
+        my $passreset = $self->db->prepare(q{UPDATE user SET pass = ? WHERE uid = ?});
+	return 1 if $passreset->execute($crptpass, $uid);
+        return undef;
+    });
+    
+    # Router #
+    ##########
+    my $r = $self->routes;
+    
+    # Normal route to controller #
+    ##############################
+    $r->route('/')->to('home#index');
+    $r->route('/signup')->to('home#signup');
+    $r->route('/signin')->to('home#signin');
+    $r->route('/signout')->to('home#signout');
+    $r->route('/passreset')->to('home#passreset');
+    $r->route('/test')->over(authenticated => 1)->to('home#test');
+    
 }
 
 1;
--- wordmedium_app/lib/WordMedium/Home.pm
+++ wordmedium_app/lib/WordMedium/Home.pm
@@ -3,7 +3,139 @@ use Mojo::Base 'Mojolicious::Controller';
 
 sub index {
 	my $self = shift;
+
 	$self->render_static('index.html');
+}
+
+sub signup {
+	my $self = shift;
+
+	my $uname  = $self->req->param('uname')  || q{};
+	my $email  = $self->req->param('email')  || q{};
+	my $pass   = $self->req->param('pass')   || q{};
+	my $repass = $self->req->param('repass') || q{};
+	my $hs     = $self->req->param('hs_email');
+	
+	if ($hs) { # robot request detected
+		$self->redirect_to('/');
+		return 1;
+	}
+	
+	unless ($uname && $email && $pass && $repass) { # please fill in all required fields
+# TODO: add flash 'please fill in all required fields'
+		$self->redirect_to('/');
+		return 1;
+	}
+	
+	if ($uname eq $pass) { # user name cannot be the same as your email 
+# TODO: add flash 'user name cannot be the same as your email'
+		$self->redirect_to('/');
+		return 1;
+	}
+
+	# check user name
+	if ($self->user2uid($uname)) { # user name already exists
+# TODO: add flash 'user name already exists'
+		$self->redirect_to('/');
+		return 1;
+	}
+	
+	# check email
+	if ($self->user2uid($email)) { # email already exists
+# TODO: add flash 'email already exists'
+		$self->redirect_to('/');
+		return 1;
+	}
+	
+	# check password match
+	if ($pass ne $repass) { # wrong password match
+# TODO: add flash 'wrong password match'
+		$self->redirect_to('/');
+		return 1;
+	}
+	
+	# create record
+	my $crptpass = $self->bcrypt($pass);
+	my $setuid = $self->db->prepare(q{INSERT INTO user VALUES ('\N', ?, ?, ?)});
+        unless ($setuid->execute($uname, $email, $crptpass)) {
+# TODO: add flash 'registration denied'
+		$self->redirect_to('/');
+#$self->app->log->debug("registration denied!\n");
+		return 1;
+	}
+	
+	$self->render(text => "USER IS CREATED!");
+	return 1;
+}
+
+sub signin {
+	my $self = shift;
+	
+	my $uname  = $self->req->param('uname')  || q{};
+	my $pass   = $self->req->param('pass')   || q{};
+	my $hs     = $self->req->param('hs_email');
+	
+	if ($hs) { # robot request detected
+		$self->redirect_to('/');
+		return 1;
+	}
+	
+	if ($self->authenticate($uname, $pass)) {
+# TODO: add redirect to members page
+		$self->render(text => "WELCOME!");
+	} else {
+		$self->redirect_to('/');
+	}
+	return 1;
+}
+
+sub signout {
+	my $self = shift;
+
+	$self->session(expires => 1);
+	$self->redirect_to('/');
+	return 1;
+}
+
+sub passreset {
+	my $self = shift;
+	
+	my $email = $self->req->param('email')  || q{};
+	my $hs    = $self->req->param('hs_email');
+	
+	if ($hs) { # robot request detected
+		$self->redirect_to('/');
+		return 1;
+	}
+	
+	my $uid = $self->user2uid($email);
+	unless ($uid) {
+# TODO: add flash 'wrong email'
+		$self->redirect_to('/');
+		return 1;
+	}
+	
+	my $domain = $self->mydomain();
+	my $new_password = $self->randpass(10); # password length = 10
+	if ($self->passreset($uid, $new_password)) {
+		$self->mail(
+			to      => $email,
+			subject => 'password recovery',
+			data    => "\nYour temporary password for $domain: $new_password\n\nPlease, change the password after signin.\n\nBest regards,\n$domain team"
+		);
+# TODO: add flash 'email sent'
+		$self->render(text => "Email was sent to $email");
+	} else {
+# TODO: add flash 'fault of password reset'
+		$self->redirect_to('/');		
+	}
+	return 1;
+}
+
+sub test {
+	my $self = shift;
+
+	$self->render(text => "TEST!");
 }
 
 1;
